7485bc467246228e1dc5d592fe684f9420f39777
rushabhN-digiqt <rushabh.navapara@digiqt.com>
2022-10-19 19:15:43 +0530

    test comit

--- curious-techno-fe/src/App.js
+++ curious-techno-fe/src/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../src/layout/navbar";
 import Navbar2 from "./layout/navbar2";
 import Footer from "./layout/footer";
@@ -15,37 +15,35 @@ import About from "./component/about";
 import YourBlog from "./users/yourBlog";
 
 function App() {
-  const [data,setData] = useState(true)
-  const getUser = (data) =>{
-    console.log(data)
-    if(localStorage.getItem('userDetails')){
-      console.log(true)
-      setData(false)
-    }else {
-      console.log(false)
-      setData(true)
+  const [data, setData] = useState(true);
+  const getUser = (data) => {
+    console.log(data);
+    if (localStorage.getItem("userDetails")) {
+      console.log(true);
+      setData(false);
+    } else {
+      console.log(false);
+      setData(true);
     }
-  }
-
-  useEffect(()=>{
-    getUser()
-  },[])
+  };
+  console.log("call");
+  useEffect(() => {
+    getUser();
+  }, []);
   return (
     <Router>
-      {
-        data ? <Navbar /> : <Navbar2 getUser={getUser}/>
-      }
+      {data ? <Navbar /> : <Navbar2 getUser={getUser} />}
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage getUser={getUser}/>} />
+        <Route path="/login" element={<LoginPage getUser={getUser} />} />
         <Route path="/sign-up" element={<CreateUser />} />
         <Route path="/home" element={<Blogs />} />
-        <Route path="/create-blog" element={<CreateBlog />} /> 
+        <Route path="/create-blog" element={<CreateBlog />} />
         <Route path="/blog-details" element={<BlogDetails />} />
         <Route path="/user-details" element={<UserDetails />} />
-        <Route path="/user-blogs" element={<YourBlog />} /> 
-        <Route path="/about" element={<About />} /> 
-        <Route path="*" element={<Errors />} /> 
+        <Route path="/user-blogs" element={<YourBlog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Errors />} />
       </Routes>
       <Footer />
     </Router>
